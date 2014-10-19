f375ae8e2d10e97f2b02e2ed072a50a63f373ddb
Lucas Santos <l-santos@live.com>
2014-10-18 23:43:41 -0300

    Primeiro Teste

--- Clinica_Odonto/src/clinica_odonto/Clinica_Odonto.java
+++ Clinica_Odonto/src/clinica_odonto/Clinica_Odonto.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package clinica_odonto;
+import Users.*;
+import java.text.*;
+import java.util.Date;
+import java.util.logging.*;
 
 /**
  *
@@ -16,7 +20,23 @@ public class Clinica_Odonto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Clinica od = new Clinica();
+        Date d = DateConverter("11/12/2014");
+        od.MarcarConsulta("1234",d);
+        od.MostrarConsultas();
         
+    }
+    
+    public static Date DateConverter(String data)
+    {
+        SimpleDateFormat x = new SimpleDateFormat("dd/MM/yyyy");
+        Date d = new  Date();
+        try {
+            d = x.parse(data);
+        } catch (ParseException ex) {
+            Logger.getLogger(Clinica_Odonto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return d;
     }
     
 }
